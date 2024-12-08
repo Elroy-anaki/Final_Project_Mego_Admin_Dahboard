0ad23ee1897a97aeb20417162f0482d1e2554d13
Elroy Anaki <elroy.a1912@gmail.com>
2024-12-08 18:12:12 +0200

    add MealSection

---
+++ AdminDashboard/src/components/tables/MealsSection/MealRow.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Pencil, Trash2 } from 'lucide-react';
+import { FaEye } from "react-icons/fa6";
+
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+
+
+function MealRow({_id, mealName, mealPrice, mealImage, ingredients, reviews}) {
+
+    console.log(_id)
+
+    const { mutate: deleteMeal} = useMutation({
+        mutationKey: ['deleteMealById'],
+        mutationFn: async (_id) => {
+            alert(_id)
+            const { data } = await axios.delete(`http://localhost:3000/meals/delete-meal-by-id/${_id}`);
+        },
+        onSuccess: (data) => alert("yes"),
+        onError: (data) => {}
+    })
+
+  return (
+    <tr key={_id} className="hover:bg-sky-100">
+            <td className="px-6 py-4 text-md font-semibold text-gray-900 text-center">{mealName}</td>
+            <td className="px-6 py-4 text-sm text-gray-900 text-center">{mealPrice}</td>
+            <td className="px-6 py-4 text-sm text-gray-900 text-center flex justify-center">
+                <img src={mealImage} alt={mealName} className='w-12 h-12 rounded-xl' />
+                
+                </td>
+            <td className="px-6 py-4 text-sm text-gray-900 text-center">-----</td>
+            <td className="px-6 py-4 text-sm text-center ">
+                <div className="inline-flex gap-3">
+                    <button
+                        className="text-blue-600 hover:text-blue-800"
+                    >
+                        <Pencil className="w-5 h-5" />
+                    </button>
+                    <button
+                    onClick={() => deleteMeal(_id)}
+                        className="text-red-600 hover:text-red-800"
+                    >
+                        <Trash2 className="w-5 h-5" />
+                    </button>
+                    <button
+                    onClick={() => deleteMeal(_id)}
+                        className="text-purple-600 hover:text-purple-800"
+                    >
+                        <FaEye className="w-5 h-5" />
+                    </button>
+                </div>
+            </td>
+        </tr>
+  )
+}
+
+export default MealRow
--- AdminDashboard/src/components/tables/MealsSection/MealsSection.jsx
+++ AdminDashboard/src/components/tables/MealsSection/MealsSection.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import MealTable from './MealTable'
+import { AddButton } from '../../common/Buttons/addButtons'
 
 function MealSection() {
   return (
-    <div className='text-5xl text-center'>
-      Meals
+    <div className='w-[75%]'>
+      <div>
+        <h1 className='text-5xl text-slate-600 text-center mx-auto mt-5'>Meals</h1>
+      </div>
+      <div className='mt-20'>
+        <MealTable />
+      </div>
+      <div className='mt-7'>
+        <AddButton text='Add Meal +' />
+      </div>
     </div>
   )
 }
