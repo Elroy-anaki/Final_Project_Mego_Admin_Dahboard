1d52247402433666bd08ff9435abb39463c357bb
Elroy Anaki <elroy.a1912@gmail.com>
2024-12-22 15:02:12 +0200

    clean the code

--- AdminDashboard/src/components/common/Buttons/EmailButton.jsx
+++ AdminDashboard/src/components/common/Buttons/EmailButton.jsx
@@ -1,28 +1,16 @@
 import { Mail } from 'lucide-react'; 
 
 const EmailButton = ({ email }) => {
-    function handleEmailClick(){
-        console.log("Email value:", email); 
-        
-        const mailtoUrl = `mailto:${email}`;
-        console.log("Mailto URL:", mailtoUrl);
-        
-        try {
-            window.location.href = mailtoUrl;
-            console.log("Location assign completed");
-        } catch (error) {
-            console.error("Error opening email:", error);
-        }
-    };
+    
 
     return (
-        <button
-            onClick={handleEmailClick}
-            className="p-1.5 text-gray-600 hover:text-sky-600 rounded-full hover:bg-sky-50 transition-colors"
+        <a
+        href={`mailto:${email}`}
+            className="p-1.5 text-purple-600 hover:text-sky-600 rounded-full hover:bg-sky-50 transition-colors"
             title={`Send email to ${email}`}
         >
             <Mail size={20} />
-        </button>
+        </a>
     );
 };
 
